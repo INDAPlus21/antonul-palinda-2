e1c2b1bd1e279339e95dd45d3f5a0a4f692eeb1a
Anton U <antonul@kth.se>
2022-03-29 20:25:55 +0200

    Bug02

--- src/bug02.go
+++ src/bug02.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 // This program should go to 11, but it seemingly only prints 1 to 10.
 func main() {
+	wg.Add(1)
 	ch := make(chan int)
 	go Print(ch)
 	for i := 1; i <= 11; i++ {
 		ch <- i
 	}
 	close(ch)
+	wg.Wait()
 }
 
 // Print prints all numbers sent on the channel.
@@ -22,4 +27,5 @@ func Print(ch <-chan int) {
 		time.Sleep(10 * time.Millisecond) // simulate processing time
 		fmt.Println(n)
 	}
+	wg.Done()
 }
