30c5ef3d4f4ec01a4b8e1823d30242476b4cfaf1
Anton U <antonul@kth.se>
2022-03-30 20:52:02 +0200

    Oracle

--- src/oracle.go
+++ src/oracle.go
@@ -42,15 +42,34 @@ func main() {
 // The oracle also prints sporadic prophecies to stdout even without being asked.
 func Oracle() chan<- string {
 	questions := make(chan string)
-	// TODO: Answer questions.
-	// TODO: Make prophecies.
-	// TODO: Print answers.
+	answers := make(chan string)
+	//prints question
+	go func() {
+		for question := range questions {
+			go prophecy(question, answers)
+		}
+	}()
+	//prints answer
+	go func() {
+		for ans := range answers {
+			fmt.Printf("%s\n>", ans)
+			//Throws you out if you ask stupid question (according to Pythia)
+			if strings.Contains(ans, " Now get lost your time is wasted here!") {
+				os.Exit(0)
+			}
+		}
+	}()
+	//random prof
+	go func() {
+		time.Sleep(time.Duration(10+rand.Intn(20)) * time.Second)
+		go prophecy("*Intense thinking*", answers)
+	}()
+
 	return questions
 }
 
 // This is the oracle's secret algorithm.
 // It waits for a while and then sends a message on the answer channel.
-// TODO: make it better.
 func prophecy(question string, answer chan<- string) {
 	// Keep them waiting. Pythia, the original oracle at Delphi,
 	// only gave prophecies on the seventh day of each month.
@@ -67,10 +86,25 @@ func prophecy(question string, answer chan<- string) {
 
 	// Cook up some pointless nonsense.
 	nonsense := []string{
-		"The moon is dark.",
-		"The sun is bright.",
+		"Love of money and nothing else will ruin Sparta",
+		"You should found a city in Libya.",
+		"Seat yourself now amidships, for you are the pilot of Athens. Grasp the helm fast in your hands; you have many allies in your city.",
+		"I count the grains of sand on the beach and measure the sea.",
+		"I understand the speech of the dumb and hear the voiceless.",
+		"The smell has come to my sense of a hard shelled tortoise boiling and bubbling with a lamb's flesh in a bronze pot: the cauldron underneath it is of bronze, and bronze is the lid.",
+		"there is no shame to be chicken-hearted.",
+		"Whenever a mule shall become sovereign king of the Medians, then, truth-seeker, flee by the stone-strewn Hermus",
+		"Get out, get out of my sanctum and drown your spirits in woe.",
 	}
-	answer <- longestWord + "... " + nonsense[rand.Intn(len(nonsense))]
+	switch rand.Intn(3) {
+	case 0:
+		answer <- "So you seek knowledge about " + longestWord + "? Well then..." + nonsense[rand.Intn(len(nonsense))]
+	case 1:
+		answer <- longestWord + " ... " + nonsense[rand.Intn(len(nonsense))]
+	default:
+		answer <- nonsense[rand.Intn(len(nonsense))] + " Now get lost your time is wasted here!"
+
+	}
 }
 
 func init() { // Functions called "init" are executed before the main function.
